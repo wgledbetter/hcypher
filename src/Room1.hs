f40923ff45636ce1f1ce019724f8e1a64e1c40fe
William G Ledbetter <williamgledbetter@gmail.com>
2022-01-08 10:33:12 -0700

    add left folding solution for r1p1

--- src/Room1.hs
+++ src/Room1.hs
@@ -2,14 +2,17 @@
 -- Some of the puzzles rely on visual effects or actually looking at the correct physical location (there's one around the back of the terminal).
 -- Those aren't code-able, but I'll write methods for the ones that can be algorithmatized.
 
-module Room1 (puzzle1, split, collectFirstChars) where
+module Room1 (puzzle1R, splitR, collectFirstCharsR, puzzle1L, splitL, collectFirstCharsL) where
 
 --------------------------------------------------------------------------------
 -- Puzzle 1 --------------------------------------------------------------------
 --------------------------------------------------------------------------------
 
-puzzle1 :: String -> String
-puzzle1 = collectFirstChars . split '\n'
+puzzle1R :: String -> String
+puzzle1R = collectFirstCharsR . splitR '\n'
+
+puzzle1L :: String -> String
+puzzle1L = collectFirstCharsL . splitL '\n'
 
 --------------------------------------------------------------------------------
 
@@ -21,16 +24,40 @@ splitFolderRight s x (l : ls)
   | s == x = "" : (l : ls) -- Split here by adding an empty string to the front of the list
   | otherwise = (x : l) : ls -- Add this character (x) to the front of the first list element
 
-split :: Char -> String -> [String]
-split _ [] = []
-split c s = foldr (splitFolderRight c) [] s
+splitR :: Char -> String -> [String]
+splitR _ [] = []
+splitR c s = foldr (splitFolderRight c) [] s
 
 --------------------------------------------------------------------------------
 
-collectFirstFolder :: String -> String -> String
-collectFirstFolder [] s = ' ' : s -- I'm assuming here that a blank line turns into a space character
-collectFirstFolder (c1 : _) [] = [c1]
-collectFirstFolder (c1 : _) s = c1 : s
+collectFirstFolderR :: String -> String -> String
+collectFirstFolderR [] s = ' ' : s -- I'm assuming here that a blank line turns into a space character
+collectFirstFolderR (c1 : _) [] = [c1]
+collectFirstFolderR (c1 : _) s = c1 : s
 
-collectFirstChars :: [String] -> String
-collectFirstChars = foldr collectFirstFolder ""
+collectFirstCharsR :: [String] -> String
+collectFirstCharsR = foldr collectFirstFolderR ""
+
+--------------------------------------------------------------------------------
+
+splitFolderLeft :: Char -> [String] -> Char -> [String]
+splitFolderLeft s [] x
+  | s == x = [""]
+  | otherwise = [[x]]
+splitFolderLeft s ll x
+  | s == x = ll ++ [""]
+  | otherwise = init ll ++ [last ll ++ [x]]
+
+splitL :: Char -> String -> [String]
+splitL _ [] = []
+splitL c s = foldl (splitFolderLeft c) [] s
+
+--------------------------------------------------------------------------------
+
+collectFirstFolderL :: String -> String -> String
+collectFirstFolderL s [] = s ++ " "
+collectFirstFolderL [] (c1 : _) = [c1]
+collectFirstFolderL s (c1 : _) = s ++ [c1]
+
+collectFirstCharsL :: [String] -> String
+collectFirstCharsL = foldl collectFirstFolderL ""
