5cdff61120c83d951825319876e3e03c185bf0e9
William G Ledbetter <williamgledbetter@gmail.com>
2022-01-09 13:34:09 -0700

    confirm correctness of r1p1

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,12 @@
 module Main where
 
 import Lib
+import Room1
 
 main :: IO ()
-main = someFunc
+main = do
+  contents <- readFile "inputs/r1p1.txt"
+  putStr $ puzzle1R contents
+  putStr "\n"
+  putStr $ puzzle1L contents
+  putStr "\n"
