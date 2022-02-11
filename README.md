a993a72f0fa63e33cd7dd1dd9cd714d2bec1b37b
William G Ledbetter <williamgledbetter@gmail.com>
2022-02-10 22:26:50 -0700

    r2p2 encoder

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # hcypher
+
+## Room 1
+
+### Puzzle 1
+
+## Room 2
+
+### Puzzle 2
+
+The current solution is sensitive to multiple spaces.
+Root cause is the implementation of `splitL` and `splitR` from Room 1.
+But it's okay for now.
+I'm happy I found a solution for N divisions instead of just 2.
--- src/Room2.hs
+++ src/Room2.hs
@@ -1,8 +1,20 @@
 -- Room 2 is about transposition
 
-module Room2 (puzzle2L, puzzle2R) where
+module Room2
+  ( puzzle2L,
+    puzzle2R,
+    puzzle2L',
+    puzzle2R',
+    transpose,
+    groupFolderRight,
+    groupIntoR,
+    groupFolderLeft,
+    groupIntoL,
+  )
+where
 
 import Room1 (splitL, splitR)
+import Prelude
 
 --------------------------------------------------------------------------------
 -- Puzzle 1 --------------------------------------------------------------------
@@ -22,6 +34,48 @@ puzzle2L = concat . transpose . splitL ' '
 puzzle2R :: String -> String
 puzzle2R = concat . transpose . splitR ' '
 
+-- Encoder (Assumes you already removed spaces from your input)
+puzzle2L' :: Int -> String -> String
+puzzle2L' n s = concat . transpose $ groupIntoL n (s ++ replicate m ' ')
+  where
+    m = (n - mod (length s) n) + n
+
+puzzle2R' :: Int -> String -> String
+puzzle2R' n s = concat . transpose $ groupIntoR n (s ++ replicate m ' ')
+  where
+    m = (n - mod (length s) n) + n
+
+--------------------------------------------------------------------------------
+
+transpose :: [[a]] -> [[a]]
+transpose ([] : _) = []
+transpose m = map head m : transpose (map tail m)
+
+--------------------------------------------------------------------------------
+
+groupFolderRight :: Int -> a -> [[a]] -> [[a]]
+groupFolderRight _ x [] = [[x]]
+groupFolderRight n x (l : ls)
+  | n > length l = (x : l) : ls
+  | otherwise = [x] : (l : ls)
+
+groupIntoR :: Int -> [a] -> [[a]]
+groupIntoR _ [] = []
+groupIntoR n x = foldr (groupFolderRight n) [] x
+
+--------------------------------------------------------------------------------
+
+groupFolderLeft :: Int -> [[a]] -> a -> [[a]]
+groupFolderLeft _ [] x = [[x]]
+groupFolderLeft n ll x
+  | n > length lll = init ll ++ [lll ++ [x]]
+  | otherwise = ll ++ [[x]]
+  where
+    lll = last ll
+
+groupIntoL :: Int -> [a] -> [[a]]
+groupIntoL _ [] = []
+groupIntoL n x = foldl (groupFolderLeft n) [] x
 
 --------------------------------------------------------------------------------
 -- Puzzle 3 --------------------------------------------------------------------
